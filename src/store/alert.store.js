e77264d81a62ba8400e812f1b57cd8ed0049f87e
ingredbarraqui <ingred.vferreira@gmail.com>
2024-11-04 12:00:29 -0300

    feat: Alert store

---
+++ src/store/alert.store.js
@@ -0,0 +1,38 @@
+import { defineStore } from 'pinia';
+
+export const useAlertStore = defineStore({
+  id: 'alert',
+  state: () => ({
+    alert: null,
+  }),
+  actions: {
+    success(message) {
+      this.alert = { 
+        message, 
+        type: 'alert-success' };
+    },
+    error(message) {
+      this.alert = { 
+        message, 
+        type: 'alert-danger' };
+    },
+    confirm(message, url) {
+      this.alert = { 
+        message, 
+        url, 
+        type: 'confirm' };
+    },
+    confirmAction(message, callback, label, fallback) {
+      this.alert = {
+        message, 
+        callback, 
+        type: 'confirmAction', 
+        label: label ?? 'OK', 
+        fallback: fallback ?? false,
+      };
+    },
+    clear() {
+      this.alert = null;
+    },
+  },
+});
