d7bad0d8bf971ed1093001b1e732e1af3627b888
ingredbarraqui <ingred.vferreira@gmail.com>
2024-11-06 14:51:21 -0300

    feat: data campo

---
+++ src/helpers/dateToFiel.js
@@ -0,0 +1,10 @@
+export default function dateToField(d) {
+  const dd = d ? new Date(d) : false;
+  return (dd) ? dd.toLocaleString('pt-BR', 
+    { 
+      dateStyle: 'short', 
+      timeStyle: 'short', 
+      timeZone: 'UTC' 
+    }
+  ) : '';
+}
